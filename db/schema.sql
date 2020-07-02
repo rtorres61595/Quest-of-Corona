7ac3b7a24d057b5d71e31954d090b3b263c7da51
ROBERT ANGEL TORRES <rtorres61595@gmail.com>
2020-07-02 19:35:24 -0400

    mysql

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS Quest_of_Corona_db_2;
-CREATE DATABASE Quest_of_Corona_db_2;
-USE Quest_of_Corona_db_2;
+DROP DATABASE IF EXISTS Quest_of_Corona_db;
+CREATE DATABASE Quest_of_Corona_db;
+USE Quest_of_Corona_db;
 
 
 
@@ -20,7 +20,6 @@ CREATE TABLE bad_guys
 	name VARCHAR(100) NOT NULL,
     health INTEGER(30) NOT NULL,
     attack INTEGER(30) NOT NULL,
-	dead BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
 
@@ -30,29 +29,46 @@ CREATE TABLE heroes
 	name VARCHAR(100) NOT NULL,
     health INTEGER(100) NOT NULL,
     attack INTEGER(30) NOT NULL,
-	heal BOOLEAN DEFAULT false,
-    dead BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE character_paths
+(
+    id int NOT NULL AUTO_INCREMENT,
+	character_class_id int NOT NULL,
+    user_id int(10) UNSIGNED NOT NULL,
+    health INTEGER(100) NOT NULL,
+    attack INTEGER(30) NOT NULL,
+	special_skill1 VARCHAR(30) NOT NULL,
+    special_skill2 VARCHAR(30) NOT NULL,
+    level INTEGER (20) NOT NULL,
+    is_complete BOOLEAN DEFAULT false,
+    currentPath VARCHAR (40) NOT NULL,
+    heal BOOLEAN DEFAULT false,
+    is_dead BOOLEAN DEFAULT false,
+    Enemy_dead BOOLEAN DEFAULT false,
 	block BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
 
-INSERT INTO bad_guys (name, health, attack, dead ) VALUES ("Covid Grunt", 10, 1, false);
+INSERT INTO bad_guys (name, health, attack ) VALUES ("Covid Grunt", 10, 1);
 
-INSERT INTO bad_guys (name, health, attack, dead ) VALUES ("Covid Monster", 10, 1, false);
+INSERT INTO bad_guys (name, health, attack ) VALUES ("Covid Monster", 10, 1);
 
-INSERT INTO bad_guys (name, health, attack, dead ) VALUES ("Covid Slime", 10, 1, false);
+INSERT INTO bad_guys (name, health, attack ) VALUES ("Covid Slime", 10, 1);
 
-INSERT INTO bad_guys (name, health, attack, dead ) VALUES ("Murder Hornet", 16, 2, false);
+INSERT INTO bad_guys (name, health, attack ) VALUES ("Murder Hornet", 16, 2);
 
-INSERT INTO bad_guys (name, health, attack, dead ) VALUES ("Covid Bat", 20, 2, false);
+INSERT INTO bad_guys (name, health, attack ) VALUES ("Covid Bat", 20, 2);
 
-INSERT INTO bad_guys (name, health, attack, dead ) VALUES ("Covid Dragon", 30, 3, false);
+INSERT INTO bad_guys (name, health, attack ) VALUES ("Covid Dragon", 30, 3);
 
 
-INSERT INTO heroes (name, health, attack, heal, dead, block ) VALUES ("Covid Knight", 45, 3, false, false , false);
+INSERT INTO heroes (name, health, attack ) VALUES ("Covid Knight", 45, 3 );
 
-INSERT INTO heroes (name, health, attack, heal, dead ,block ) VALUES ("Covid Nnja", 30, 4, false, false , false);
+INSERT INTO heroes (name, health, attack ) VALUES ("Covid Nnja", 30, 4 );
 
-INSERT INTO heroes (name, health, attack, heal, dead ,block ) VALUES ("Covid Mage", 40, 3, false, false , false);
+INSERT INTO heroes (name, health, attack ) VALUES ("Covid Mage", 40, 3 );
 
-INSERT INTO heroes (name, health, attack, heal, dead ,block ) VALUES ("Covid Warlock", 30, 4, false, false, false);
+INSERT INTO heroes (name, health, attack ) VALUES ("Covid Warlock", 30, 4 );
