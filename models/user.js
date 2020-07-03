2ce6668e0c2a555c65812f178c0ca62e48308421
Abigail Limpioso <alimpioso@gmail.com>
2020-07-02 20:53:15 -0400

    updated models with schema colums

--- models/user.js
+++ models/user.js
@@ -17,7 +17,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-    username: {
+    user_name: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
